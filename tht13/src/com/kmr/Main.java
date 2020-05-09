8cb7a014f18b812e758087a7e17e9e9c0426bdef
Walter Ruoppa <walter.ruoppa@metropolia.fi>
2020-05-09 23:09:07 +0300

    done

--- tht13/src/com/kmr/Main.java
+++ tht13/src/com/kmr/Main.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Give hashing size");
-        ClosedHashing closedHashing = new ClosedHashing(lukija.nextInt());
+        ClosedHashing closedHashing = new ClosedHashing(sc.nextInt());
 
         char input;
         do {
             int indeksi;
 
             System.out.println("0: Quit\n1: Add key\n2: Search key\n3: print detail");
-            input = lukija.next().charAt(0);
+            input = sc.next().charAt(0);
             switch (input) {
                 case '1':
                     System.out.println("Give index of a key");
-                    indeksi = lukija.nextInt();
+                    indeksi = sc.nextInt();
                     System.out.println("Give key detail.");
-                    closedHashing.add(indeksi, lukija.next());
+                    closedHashing.add(indeksi, sc.next());
                     break;
                 case '2':
                     System.out.println("Give searched key");
-                    indeksi = lukija.nextInt();
+                    indeksi = sc.nextInt();
                     System.out.println(closedHashing.searchKey(indeksi));
                     break;
                 case '3':
@@ -36,6 +36,6 @@ public class Main {
             }
         } while (input != '0');
 
-        lukija.close();
+        sc.close();
     }
 }
